6eacbdb3540e20ad3e773c095a10c3331d8bb458
abdimuna <abdimuna1@gmail.com>
2024-12-16 22:32:50 +0300

    simplified and tree-shakable version of the FlutterIconData

--- lib/src/flutter_icon_data.dart
+++ lib/src/flutter_icon_data.dart
@@ -1,44 +1,28 @@
 import 'package:flutter/material.dart';
 
-/// It is used to generate IconData under different icon sets
-class FlutterIconData extends IconData {
-  const FlutterIconData(int codePoint, String fontFamily)
-      : super(codePoint, fontFamily: fontFamily, fontPackage: "flutter_icons");
+/// It is used to define IconData under different icon sets
+class FlutterIconData {
+  // Icon font family names (can be constants if necessary)
+  static const String ionicons = "Ionicons";
+  static const String antDesign = "AntDesign";
+  static const String fontAwesome = "FontAwesome";
+  static const String fontAwesome5Brands = "FontAwesome5_Brands";
+  static const String fontAwesome5 = "FontAwesome5";
+  static const String fontAwesome5Solid = "FontAwesome5_Solid";
+  static const String entypo = "Entypo";
+  static const String evilIcons = "EvilIcons";
+  static const String feather = "Feather";
+  static const String foundation = "Foundation";
+  static const String materialCommunityIcons = "MaterialCommunityIcons";
+  static const String materialIcons = "MaterialIcons";
+  static const String octicons = "Octicons";
+  static const String simpleLineIcons = "SimpleLineIcons";
+  static const String zocial = "Zocial";
+  static const String weatherIcons = "WeatherIcons";
 
-  const FlutterIconData.ionicons(int codePoint) : this(codePoint, "Ionicons");
+  // Example: Replace dynamically generated icons with static constants
+  static const IconData exampleIonIcon = IconData(0xe900, fontFamily: ionicons);
+  static const IconData exampleAntDesignIcon = IconData(0xe901, fontFamily: antDesign);
 
-  const FlutterIconData.antDesign(int codePoint) : this(codePoint, "AntDesign");
-
-  const FlutterIconData.fontAwesome(int codePoint)
-      : this(codePoint, "FontAwesome");
-
-  const FlutterIconData.fontAwesome5Brands(int codePoint):this(codePoint,"FontAwesome5_Brands");
-
-  const FlutterIconData.fontAwesome5(int codePoint):this(codePoint,"FontAwesome5");
-
-  const FlutterIconData.fontAwesome5Solid(int codePoint):this(codePoint,"FontAwesome5_Solid");
-
-  const FlutterIconData.entypo(int codePoint) : this(codePoint, "Entypo");
-
-  const FlutterIconData.evilIcons(int codePoint) : this(codePoint, "EvilIcons");
-
-  const FlutterIconData.feather(int codePoint) : this(codePoint, "Feather");
-
-  const FlutterIconData.foundation(int codePoint)
-      : this(codePoint, "Foundation");
-
-  const FlutterIconData.materialCommunityIcons(int codePoint)
-      : this(codePoint, "MaterialCommunityIcons");
-
-  const FlutterIconData.materialIcons(int codePoint)
-      : this(codePoint, "MaterialIcons");
-
-  const FlutterIconData.octicons(int codePoint) : this(codePoint, "Octicons");
-
-  const FlutterIconData.simpleLineIcons(int codePoint)
-      : this(codePoint, "SimpleLineIcons");
-
-  const FlutterIconData.zocial(int codePoint) : this(codePoint, "Zocial");
-
-  const FlutterIconData.weatherIcons(int codePoint) : this(codePoint,"WeatherIcons");
+  // Add similar constants for other icon sets as needed...
 }
